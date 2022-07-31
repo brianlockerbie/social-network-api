cd2a6d13e1498ab4513bf4edf46ff003b7f2399a
Brian <brian.lockerbie@gmail.com>
2022-07-31 15:51:32 -0700

    added add & delete friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,7 +72,49 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
-    }
+    },
+ 
+        addFriend({ params }, res) {
+            User.findOneAndUpdate(
+                { _id: params.id },
+                { $push: { friends: params.friendId }},
+                { new: true, runValidators: true }
+            )
+            .populate({
+                path: 'friends',
+                select: ('-__v')
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Invalid id. No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+                })
+                .catch(err => res.json(err));
+        },
+
+        deleteFriend({ params }, res) {
+            User.findOneAndUpdate(
+                { _id: params.id }, 
+                { $pull: { friends: params.friendId }},
+                { new: true }
+            )
+            .populate({
+                path: 'friends', 
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'Id not valid. No User found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+        }
 };
 
 module.exports = userController;
