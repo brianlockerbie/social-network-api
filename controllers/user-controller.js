f3d9cf4c981afe1ffc7cb1948b50a738d3ab496e
Brian <brian.lockerbie@gmail.com>
2022-07-31 11:31:44 -0700

    fixed user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,4 +18,30 @@ const userController = {
                 console.log(err);
                 res.status(500).json(err);
             });
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({__id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        // return if no user is found 
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!'});
+                return; 
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+},
     }
